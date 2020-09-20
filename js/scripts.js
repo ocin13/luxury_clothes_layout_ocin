7be48df025d9f90069a9d4a6c1fb9758f34054dd
Hocine <hoc1ne13dec@gmail.com>
2020-09-20 11:41:16 -0400

    luxury-clothes-001

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#open").click(function(){
+        $("#mySidebar").hasClass("element-close");
+            $("#mySidebar").removeClass("element-close");
+            $("#mySidebar").addClass("element-open");
+            $("#open").addClass("element-close");
+        
+    });
+    $("#close").click(function(){
+        
+            $("#mySidebar").removeClass("element-open");
+            $("#mySidebar").addClass("element-close");
+            $("#open").removeClass("element-close");
+        
+    });
+    $(".menu-item").click(function(){
+        
+        $("#mySidebar").removeClass("element-open");
+        $("#mySidebar").addClass("element-close");
+        $("#open").removeClass("element-close");
+    
+});
+    $("#men").click(function(){
+        $(".women").addClass("element-close");
+        $(".men").removeClass("element-close");
+    });
+    $("#women").click(function(){
+        $(".men").addClass("element-close");
+        $(".women").removeClass("element-close");
+    });
+    $("#all").click(function(){
+        $(".men").removeClass("element-close");
+        $(".women").removeClass("element-close");
+    });
+    $(window).scroll(function(){
+        $(".go-top").toggleClass("up-active")
+    });
+});
